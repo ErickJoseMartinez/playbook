1b0c62c0f77dabc130bc43f8eb4dfc7ba27094de
ErickJoseMartinez <launchx08173@innovaccion.mx>
2022-04-18 18:38:44 -0500

    Prueba Live 2

---
+++ weekly_mission_2/example_1/example1_1.js
@@ -0,0 +1,19 @@
+console.log("Alooo")
+
+const myFirstProject = {}
+
+console.log(`1. Objeto vacío: ${myFirstProject}`)
+
+console.log(myFirstProject)
+
+const alumno ={
+    name: "Erick",
+    first_name:"Martinez",
+    last_name:"Lopez",
+    blog:{
+        url:"https://"
+    }
+}
+
+console.log(alumno)
+console.log(alumno.name)
