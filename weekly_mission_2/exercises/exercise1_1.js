06e76d9306347bfc46aa6e6b5ad3337cd51edb79
ErickJoseMartinez <launchx08173@innovaccion.mx>
2022-04-21 11:55:38 -0500

    2.0 Ejercicio modelado de sistemas

--- weekly_mission_2/exercises/exercise1_1.js
+++ weekly_mission_2/exercises/exercise1_1.js
@@ -42,4 +42,96 @@ const twitter_trending = {
             loves: 1
         }
 }
-    //Twitter
+//Facebook
+const facebook_user = {
+    name: "Erick Jose",
+    first_name: "Martinez",
+    last_name:  "Lopez",
+    username: "@erickmalo02",
+    password: "aksgd123",
+    date_birth: "1993-07-02",
+    gender: "male",
+    email:"erickmalo02@gmail.com",
+    number: "5535258306"
+}
+
+const facebook_biography = {
+    username: "@erickmalo02",
+    name: "Erick Jose",
+    studies:{
+        school: "UPIICSA"
+    },
+    live: "cdmx",
+    civil_status: "SOLTERO",
+    number: "5535258306",
+    hobby:{
+        name: "Music"
+    }
+}
+
+const facebook_post = {
+    name: "Erick Jose",
+    group: 1,
+    message: "Este post es para publicar acerca de tips en la programación",
+    post:{
+        foto_video:{
+            status: false,
+            data: "https://imagen/1.png"
+        },
+        feels:{
+            status: true,
+            data: 1
+        },
+        questions_answers:{
+            status: false,
+            data: null
+        },
+        gif:{
+            status: false,
+            data: null
+        },
+        person:{
+            status: false,
+            data: null
+        },
+        here:{
+            status: true,
+            data: "CDMX, México"
+        },
+        important:{
+            status: false,
+            data: null
+        },
+        video_live:{
+            status: false,
+            data: null
+        }
+    }
+}
+
+// UBER
+const uber_user = {
+    name: "Erick Jose",
+    first_name: "Martinez",
+    last_name:  "Lopez",
+    username: "@erickmalo02",
+    password: "aksgd123",
+    date_birth: "1993-07-02",
+    gender: "male",
+    email:"erickmalo02@gmail.com",
+    number: "5535258306"
+}
+const uber_travel = {
+    date: "2022-04-18",
+    car_id: "PBR-21-31",
+    mount: "199.99",
+    currency: "MXN",
+    direction_initial: "NORTE 70 A ...",
+    direction_final: "Cerrada 27",
+    stars: 5,
+    map:{
+        data: "https:google.maps/1"
+    }
+}
+
+console.log(uber_travel.map.data)
