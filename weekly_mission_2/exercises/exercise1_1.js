2aab71570f22d54abe1dd8758d0289b9a6507820
ErickJoseMartinez <launchx08173@innovaccion.mx>
2022-04-21 10:31:09 -0500

    Ejercicio Twitter

---
+++ weekly_mission_2/exercises/exercise1_1.js
@@ -0,0 +1,45 @@
+//Twitter
+    //user const user = {user: "carlo", username: "carlogilmar", bio: "...", age: 23}
+    //trending_topic
+    //hashtag
+//Facebook
+    //user
+    //post
+    //biography
+//Uber
+    //profile
+    //travel
+
+    //Twitter
+const twitter_user = {
+    name: "Erick Jose",
+    first_name: "Martinez",
+    last_name:  "Lopez",
+    username: "@erickmalo02",
+    location: "cdmx",
+    site: "https://github.com/ErickJoseMartinez",
+    age: "28",
+    biography: "Este usuario buscar convertirse en un gran programador",
+    creation_date: "2016-01-01"
+}
+
+const twitter_trending = {
+    author: "Erick Jose Martinez Lopez",
+    username: "@erickmalo02",
+    photo: "https://github.com/ErickJoseMartinez/img/profile.png",
+    title: "Este twitt solo es para motivarme día con día",
+    likes: 1000,
+    retwitt: 10,
+    loves: 1001,
+        comment:{
+            id: 10001,
+            author_comment: "Erick Jose Martinez Lopez",
+            username_comment: "@erickmalo02",
+            photo_comment: "https://github.com/ErickJoseMartinez/img/profile.png",
+            answer: "Motivate mi chavo",
+            likes: 1,
+            retwitt: 1,
+            loves: 1
+        }
+}
+    //Twitter
